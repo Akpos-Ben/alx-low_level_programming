9c4169787f60afc3958aa0e2ee250eb8a843e372
Akpos-Ben <bernardodjaremu@gmail.com>
2023-10-30 19:38:04 -0700

    God is Good

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,29 +9,22 @@
  */
 char *_strdup(char *str)
 {
-    char *aaa;
-    int i, r = 0;
-
-    if (str == NULL)
-        return (NULL);
-
-    // Calculate the length of the input string 'str'
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-
-    // Allocate memory for the new string 'aaa', including space for the null terminator
-    aaa = malloc(sizeof(char) * (i + 1));
-
-    if (aaa == NULL)
-        return (NULL);
-
-    // Copy the characters from 'str' to 'aaa'
-    for (r = 0; str[r]; r++)
-        aaa[r] = str[r];
-
-    // Null-terminate the duplicated string
-    aaa[r] = '\0';
-
-    return (aaa);
+char *aaa;
+int i, r = 0;
+if (str == NULL)
+return (NULL);
+/* Calculate the length of the input string 'str' */
+i = 0;
+while (str[i] != '\0')
+++;
+/* Allocate memory for the new string 'aaa', including space for the null terminator */
+aaa = (char *)malloc(sizeof(char) * (i + 1));
+if (aaa == NULL)
+return (NULL);
+/* Copy the characters from 'str' to 'aaa' */
+for (r = 0; str[r]; r++)
+aaa[r] = str[r];
+/* Null-terminate the duplicated string */
+aaa[r] = '\0';
+return (aaa);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,40 +9,38 @@
  */
 int **alloc_grid(int width, int height)
 {
-    int **mee;  // Pointer to a pointer for the 2D grid
-    int x, y;   // Loop counters
+int **mee;  / Pointer to a pointer for the 2D grid
+int x, y;   / Loop counters
 
-    // Check if width or height is non-positive
-    if (width <= 0 || height <= 0)
-        return (NULL);
+/ Check if width or height is non - positive
+if (width <= 0 || height <= 0)
+return (NULL);
 
-    // Allocate memory for an array of int pointers (rows)
-    mee = malloc(sizeof(int *) * height);
+/ Allocate memory for an array of int pointers(rows)
+mee = malloc(sizeof(int *) * height);
 
-    if (mee == NULL)
-        return (NULL);
+if (mee == NULL)
+return (NULL);
 
-    // Allocate memory for each row (array of ints)
-    for (x = 0; x < height; x++)
-    {
-        mee[x] = malloc(sizeof(int) * width);
+/ Allocate memory for each row(array of ints)
+for (x = 0; x < height; x++)
+{
+mee[x] = malloc(sizeof(int) * width);
 
-        if (mee[x] == NULL)
-        {
-            // Free previously allocated memory on failure
-            for (; x >= 0; x--)
-                free(mee[x]);
-            free(mee);
-            return (NULL);
-        }
-    }
-
-    // Initialize the elements of the grid to 0
-    for (x = 0; x < height; x++)
-    {
-        for (y = 0; y < width; y++)
-            mee[x][y] = 0;
-    }
-
-    return (mee);  // Return a pointer to the 2D array
+if (mee[x] == NULL)
+{
+/ Free previously allocated memory on failure
+for (; x >= 0; x--)
+free(mee[x]);
+free(mee);
+return (NULL);
+}
+}
+/ Initialize the elements of the grid to 0
+for (x = 0; x < height; x++)
+{
+for (y = 0; y < width; y++)
+mee[x][y] = 0;
+}
+return (mee);  / Return a pointer to the 2D array
 }
