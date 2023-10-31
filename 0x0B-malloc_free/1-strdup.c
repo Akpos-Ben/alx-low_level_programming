855f5cd6b4e8dc494e4232c8d710dba268f78713
Akpos-Ben <bernardodjaremu@gmail.com>
2023-10-30 20:15:14 -0700

    JesuOvie is blessed

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,57 @@
 #include <stdio.h>
+		
 #include <stdlib.h>
+		
 #include "main.h"
-
+		
 /**
- * _strdup - duplicate a string to a new memory space location
- * @str: a pointer to a string (char *)
- * Return: a pointer to the duplicated string or NULL if allocation fails
+		
+ * _strdup - duplicate to new memory space location
+		
+ * @str: char
+		
+ * Return: 0
+		
  */
+		
 char *_strdup(char *str)
+		
 {
-char *aaa;
-int i, r = 0;
-if (str == NULL)
-return (NULL);
-/* Calculate the length of the input string 'str' */
-i = 0;
-while (str[i] != '\0')
-++;
-/* Allocate memory for the new string 'aaa', including space for the null terminator */
-aaa = (char *)malloc(sizeof(char) * (i + 1));
-if (aaa == NULL)
-return (NULL);
-/* Copy the characters from 'str' to 'aaa' */
-for (r = 0; str[r]; r++)
-aaa[r] = str[r];
-/* Null-terminate the duplicated string */
-aaa[r] = '\0';
-return (aaa);
+		
+	char *aaa;
+		
+	int i, r = 0;
+		
+
+		
+	if (str == NULL)
+		
+		return (NULL);
+		
+	i = 0;
+		
+	while (str[i] != '\0')
+		
+		i++;
+		
+
+		
+	aaa = malloc(sizeof(char) * (i + 1));
+		
+
+		
+	if (aaa == NULL)
+		
+		return (NULL);
+		
+
+		
+	for (r = 0; str[r]; r++)
+		
+		aaa[r] = str[r];
+		
+
+		
+	return (aaa);
+		
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,46 +1,85 @@
 #include "main.h"
+		
 #include <stdlib.h>
-
+		
 /**
- * alloc_grid - dynamically allocate memory for a 2D grid
+		
+ * alloc_grid - nested loop to make grid
+		
  * @width: width input
+		
  * @height: height input
- * Return: pointer to a 2D array or NULL if allocation fails
+		
+ * Return: pointer to 2 dim. array
+		
  */
+		
 int **alloc_grid(int width, int height)
+		
 {
-int **mee;  / Pointer to a pointer for the 2D grid
-int x, y;   / Loop counters
+		
+	int **mee;
+		
+	int x, y;
+		
 
-/ Check if width or height is non - positive
-if (width <= 0 || height <= 0)
-return (NULL);
+		
+	if (width <= 0 || height <= 0)
+		
+		return (NULL);
+		
 
-/ Allocate memory for an array of int pointers(rows)
-mee = malloc(sizeof(int *) * height);
+		
+	mee = malloc(sizeof(int *) * height);
+		
 
-if (mee == NULL)
-return (NULL);
+		
+	if (mee == NULL)
+		
+		return (NULL);
+		
 
-/ Allocate memory for each row(array of ints)
-for (x = 0; x < height; x++)
-{
-mee[x] = malloc(sizeof(int) * width);
+		
+	for (x = 0; x < height; x++)
+		
+	{
+		
+		mee[x] = malloc(sizeof(int) * width);
+		
 
-if (mee[x] == NULL)
-{
-/ Free previously allocated memory on failure
-for (; x >= 0; x--)
-free(mee[x]);
-free(mee);
-return (NULL);
-}
-}
-/ Initialize the elements of the grid to 0
-for (x = 0; x < height; x++)
-{
-for (y = 0; y < width; y++)
-mee[x][y] = 0;
-}
-return (mee);  / Return a pointer to the 2D array
+		
+		if (mee[x] == NULL)
+		
+		{
+		
+			for (; x >= 0; x--)
+		
+				free(mee[x]);
+		
+
+		
+			free(mee);
+		
+			return (NULL);
+		
+		}
+		
+	}
+		
+
+		
+	for (x = 0; x < height; x++)
+		
+	{
+		
+		for (y = 0; y < width; y++)
+		
+			mee[x][y] = 0;
+		
+	}
+		
+
+		
+	return (mee);
+		
 }
